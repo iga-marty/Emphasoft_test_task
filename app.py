855b3ce37ecd4366de2bd47c6382d140aba42f1e
iga-marty <martynenko.basis@gmail.com>
2024-08-16 17:17:20 +0300

    adding celery 1

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
+import os
 import multiprocessing as mp
 from flask import Flask, render_template
 
 from random import randint
 from time import sleep
 
+from dotenv import load_dotenv
+from celery import Celery
+
+
+load_dotenv()
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'top secret!'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['OAUTH2_PROVIDERS'] = {
+    'github': {
+        'client_id': os.environ.get('GITHUB_CLIENT_ID'),
+        'client_secret': os.environ.get('GITHUB_CLIENT_SECRET'),
+        'authorize_url': 'https://github.com/login/oauth/authorize',
+        'token_url': 'https://github.com/login/oauth/access_token',
+        'userinfo': {
+            'url': 'https://api.github.com/user/emails',
+            'email': lambda json: json[0]['email'],
+        },
+        'scopes': ['user:email'],
+    },
+}
+
+
+celery = Celery(app.name)
+
+@celery.task
+def creating_random_value():
+    pass
 
 
 def get_app():
